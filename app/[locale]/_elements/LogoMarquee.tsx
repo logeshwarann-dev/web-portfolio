3005358ae807bffd9e83bdab5ab5308fdc0a5f61
Logeshwaran N <logeshwaran082@gmail.com>
2025-07-11 14:57:34 +0530

    fixed resume download error

--- app/[locale]/_elements/LogoMarquee.tsx
+++ app/[locale]/_elements/LogoMarquee.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Marquee from "react-fast-marquee";
+import KafkaLogo from "../../../public/logos/kafka.svg"
 import KubernetesLogo from "../../../public/logos/kubernetes.svg";
 import NextJSLogo from "../../../public/logos/nextjs.svg";
 
@@ -9,7 +10,7 @@ export default function LogoMarquee({ className }: Readonly<LogoMarqueeProps>) {
     return (
         <div className={className}>
             <Marquee direction="left" gradient speed={40} gradientWidth={48}>
-                
+                <KafkaLogo className="h-10 px-2 mx-6" />
                 <KubernetesLogo className="h-10 px-2 mx-6" />
                 <NextJSLogo className="h-10 px-2 mx-6" />
             </Marquee>
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,3 +1,3 @@
-export const resumeLink = `/${encodeURI("Resume_LogeshwaranN.pdf")}`;
+export const resumeLink = `/${encodeURI("ResumeLogeshwaranN.pdf")}`;
 export const isProduction = process.env.NODE_ENV === "production";
 export const baseUrl = isProduction ? "https://logeshwarann.info" : "http://localhost:3000";
